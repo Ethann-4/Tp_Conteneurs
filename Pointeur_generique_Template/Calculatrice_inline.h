7394206c0294e329f33d757f6021c9cc078d85af
Ethann-4 <ethann2906@gmail.com>
2025-09-22 14:53:14 +0200

    Update Calculatrice_inline.h

--- Pointeur_generique_Template/Calculatrice_inline.h
+++ Pointeur_generique_Template/Calculatrice_inline.h
@@ -22,6 +22,6 @@ public:
 		cout << "Soustraction   : " << soustraction() << endl;
 		cout << "Multiplication : " << multiplication() << endl;
 		cout << "Division       : " << division() << endl;
-		cout << "---------------------------" << endl;
+		
 	}
 };
